7ef4ab1512667117eb2bc294bf0182ef612197df
matryoshka <nazardjali@gmail.com>
2017-09-28 22:25:01 +0300

    initital

---
+++ Cardbooru/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace Cardbooru
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
