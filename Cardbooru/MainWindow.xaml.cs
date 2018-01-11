c1fb98a790d4377b3795b2323b8bf00e8551ec92
matryoshka <nazardjali@gmail.com>
2018-01-11 17:32:09 +0200

    Load datacontex through xaml

--- Cardbooru/MainWindow.xaml.cs
+++ Cardbooru/MainWindow.xaml.cs
@@ -8,24 +8,10 @@ namespace Cardbooru
     public partial class MainWindow : Window
     {
 
-        private MainWidowViewModel model = new MainWidowViewModel();
-
         public MainWindow()
         {
-            InitializeComponent();
-            DataContext = model;
-            
-            
+            InitializeComponent();      
         }
 
-        private  async void Action(object sender, RoutedEventArgs e) {
-           //await model.FillBooruImages(1);
-           
-           Console.WriteLine(model.BooruImages.Count.ToString());
-        }
-
-        private void Action2(object sender, RoutedEventArgs e) {
-            
-        }
     }
 }
---
+++ Cardbooru/MainWindowViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cardbooru
+{
+    public class MainWidowViewModel : INotifyPropertyChanged {
+        private int currentPage;
+
+        private BooruWorker booruWorker; // TODO Make Interface
+
+        public ObservableCollection<BooruImage> BooruImages { get; set; } = 
+            new ObservableCollection<BooruImage>();
+        
+
+        public MainWidowViewModel() {
+            currentPage = 1;
+            booruWorker = new BooruWorker();
+        }
+
+
+        #region Commands
+
+        private RelayCommand loadPreviewImages;
+        public RelayCommand LoadCommand => loadPreviewImages ?? 
+            (loadPreviewImages = new RelayCommand(async o => {
+                await booruWorker.FillBooruImages(currentPage, BooruImages);
+            }));
+
+        #endregion
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
