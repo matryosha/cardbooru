9151dbb810c942a8fddd34d88d188ca68a8d4bd1
matryoshka <nazardjali@gmail.com>
2018-01-19 18:39:20 +0200

    base class for booru images

---
+++ Cardbooru/Models/Base/BooruImageModeBasel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Cardbooru.Models.Base
+{
+    public abstract class BooruImageModelBase :
+        INotifyPropertyChanged
+    {
+        private string _hash;
+        private Image _previewImage;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public virtual string Id { get; set; }
+        public virtual string PreviewImageUrl { get; set; }
+        public virtual string FullImageUrl { get; set; }
+        public virtual Image FullImage { get; set; }
+        public virtual string Hash {
+            get => _hash;
+            set {
+                _hash = value;
+                OnPropertyChanged("Hash");
+            }
+        }
+
+        public virtual Image PreviewImage {
+            get => _previewImage;
+            set {
+                _previewImage = value;
+                OnPropertyChanged("PreviewImage");
+                OnPropertyChanged("PreviewImageSource");
+            }
+        }
+
+        public ImageSource PreviewImageSource {
+            get => PreviewImage.Source;
+            set {
+                if (value == null) throw new ArgumentNullException(nameof(value));
+                PreviewImageSource = value;
+                OnPropertyChanged("PreviewImageSource");
+            }
+        }
+
+
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Cardbooru/Models/DanbooruImageModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Controls;
+using Cardbooru.Models.Base;
+using Newtonsoft.Json;
+
+
+namespace Cardbooru.Models
+{
+    public class DanbooruImageModel :
+        BooruImageModelBase
+    {
+        [JsonProperty("id")]
+        public override string Id { get => base.Id; set => base.Id = value; }
+        [JsonProperty("preview_file_url")]
+        public override string PreviewImageUrl { get => base.PreviewImageUrl; set => base.PreviewImageUrl = value; }
+        [JsonProperty("large_file_url")]
+        public override string FullImageUrl { get => base.FullImageUrl; set => base.FullImageUrl = value; }
+        [JsonProperty("md5")]
+        public override string Hash { get => base.Hash; set => base.Hash = value; }
+        public override Image PreviewImage { get => base.PreviewImage; set => base.PreviewImage = value; }
+        public override Image FullImage { get => base.FullImage; set => base.FullImage = value; }
+    }
+}
