0b4494133a56edf936b7a68d34973fce35e02359
matryoshka <nazardjali@gmail.com>
2018-03-16 15:28:05 +0200

    some new methods for FullModel

--- Cardbooru/FullImageBrowsing/FullImageBrowsingViewModel.cs
+++ Cardbooru/FullImageBrowsing/FullImageBrowsingViewModel.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 using System.Windows.Media;
 using Cardbooru.Helpers;
 using Cardbooru.Helpers.Base;
@@ -13,6 +15,7 @@ namespace Cardbooru.FullImageBrowsing
     class FullImageBrowsingViewModel : 
         INotifyPropertyChanged, IUserControlViewModel {
 
+        public event PropertyChangedEventHandler PropertyChanged;
         public IMvxMessenger Messenger { get; }
         public List<string> TagsList { get; set; }
 
@@ -34,23 +37,7 @@ namespace Cardbooru.FullImageBrowsing
             } 
         }
 
-        private ObservableCollection<BooruImageModelBase> _booruImagesCollection;
-
-        public FullImageBrowsingViewModel(BooruImageModelBase booruImageModel) {
-            Messenger = IdkInjection.MessengerHub;
-            BooruImageModel = booruImageModel;
-            TagsList = booruImageModel.TagsList;
-        }
-
-        public FullImageBrowsingViewModel(BooruImageModelBase booruImageModel, ObservableCollection<BooruImageModelBase> collection = null) {
-            Messenger = IdkInjection.MessengerHub;
-            BooruImageModel = booruImageModel;
-            _booruImagesCollection = collection;
-            TagsList = booruImageModel.TagsList;
-        }
-
         private RelayCommand _closeImageCommand;
-
         public RelayCommand CloseImageCommand {
             get => _closeImageCommand ?? (_closeImageCommand = new RelayCommand(o => {
                 BooruImageModel.FullImage = null;
@@ -59,7 +46,44 @@ namespace Cardbooru.FullImageBrowsing
                 }));
         }
 
-        public event PropertyChangedEventHandler PropertyChanged;
+        private RelayCommand _nextImage;
+        public  RelayCommand NextImage {
+            get => _nextImage ?? (_nextImage = new RelayCommand( async o => {
+                BooruImageModel.IsFullImageLoaded = false;
+                if(_currentImageIndex == -1)
+                    await Task.Run(() => FindCurrentImageIndex());
+                if(_currentImageIndex >= _booruImagesCollection.Count) throw new Exception("Not Implement yet");
+                
+                BooruImageModel = _booruImagesCollection[++_currentImageIndex];
+            }));
+        }
+
+        private RelayCommand _prevImage;
+        public RelayCommand PrevImage {
+            get => _prevImage ?? (_prevImage = new RelayCommand(o => { }));
+        }
+
+
+        private ObservableCollection<BooruImageModelBase> _booruImagesCollection;
+        private int _currentImageIndex = -1;
+
+
+        public FullImageBrowsingViewModel(BooruImageModelBase booruImageModel) {
+            Messenger = IdkInjection.MessengerHub;
+            BooruImageModel = booruImageModel;
+            TagsList = booruImageModel.TagsList;
+        }
+
+        public FullImageBrowsingViewModel(BooruImageModelBase booruImageModel, ObservableCollection<BooruImageModelBase> collection) {
+            Messenger = IdkInjection.MessengerHub;
+            BooruImageModel = booruImageModel;
+            _booruImagesCollection = collection;
+            TagsList = booruImageModel.TagsList;
+        }
+
+        private void FindCurrentImageIndex() {
+            _currentImageIndex = _booruImagesCollection.IndexOf(BooruImageModel);
+        }
 
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null) {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
