602a01212acf9b7df161b3c777589492e8e2f645
matryoshka <nazardjali@gmail.com>
2020-04-06 05:31:29 -0700

    Update BooruImage
    Changed BitmapImage type member to byte[]

--- Cardbooru.Application/Entities/BooruImage.cs
+++ Cardbooru.Application/Entities/BooruImage.cs
@@ -1,10 +1,8 @@
-﻿using System.Windows.Media.Imaging;
-
-namespace Cardbooru.Application.Entities
+﻿namespace Cardbooru.Application.Entities
 {
     public class BooruImage
     {
         public string Hash { get; set; }
-        public BitmapImage Image { get; set; }
+        public byte[] Data { get; set; }
     }
 }
